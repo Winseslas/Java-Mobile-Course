2f99347ce9a78bf6673924591e6cf11dbd6b6a0c
Winseslas <eyebejoel35@gmail.com>
2023-03-25 15:33:10 +0100

    error

---
+++ app/src/main/java/com/winseslas/refactoring/MainActivity.java
@@ -0,0 +1,27 @@
+package com.winseslas.refactoring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.winseslas.refactoring.navigation.DrawerNavigationActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+//        setTitle("You are welcome");
+//
+//        Button getStartingButton = findViewById(R.id.getStarting);
+//        getStartingButton.setOnClickListener(v -> {
+//            Intent registerActivity = new Intent(getApplicationContext(), DrawerNavigationActivity.class);
+//            startActivity(registerActivity);
+//            finish();
+//        });
+
+    }
+}
